6e86115185f3f93b9812e44869283c92603fe0c2
Vitaly <matviuk@gmail.com>
2018-03-22 00:36:54 +0300

    feat: add styles for restaurants list

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute('alt', restaurant.name);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
